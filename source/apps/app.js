77bf842eaf784ea065360f2a2798b2ed0b8b9b35
Dave Freeman <sugardave@gmail.com>
2013-04-24 11:42:24 -0500

    simplified controllers for now

--- source/apps/app.js
+++ source/apps/app.js
@@ -3,17 +3,11 @@ enyo.kind({
 	kind: "enyo.Application",
 	controllers: [
 		{name: "storage", kind: "PropertyCross.StorageController"},
-		{name: "results", kind: "PropertyCross.ResultsController"},
 		{name: "panels", kind: "PropertyCross.PanelsController"},
-		{name: "listing", kind: "PropertyCross.ListingController"},
-		{name: "listings", kind: "PropertyCross.ListingsController"},
-		//{name: "favorites", kind: "PropertyCross.FavoritesController"}
+		{name: "results", kind: enyo.Controller},
+		{name: "listing", kind: enyo.Controller},
+		{name: "listings", kind: enyo.Controller},
 		{name: "favorites", kind: enyo.Controller}
 	],
-	view: "PropertyCross.MainView",
-	addRecord: function (sender, event) {
-		var data = this.controllers.listing.get("data");
-		var listings = this.controllers.listings;
-		listings.add({message: data});
-	}
+	view: "PropertyCross.MainView"
 });
--- source/controllers/controllers.js
+++ source/controllers/controllers.js
@@ -1,28 +1,4 @@
 enyo.ready(function () {
 
-	enyo.kind({
-		name: "PropertyCross.LocationsController",
-		kind: enyo.ArrayController
-	});
 	
-	enyo.kind({
-		name: "PropertyCross.ListingController",
-		kind: enyo.Controller,
-		data: "DEFAULT LISTING"
-	});
-
-	enyo.kind({
-		name: "PropertyCross.ListingsController",
-		kind: enyo.Controller
-	});
-
-	enyo.kind({
-		name: "PropertyCross.ResultsController",
-		kind: enyo.Controller
-	});
-
-	enyo.kind({
-		name: "PropertyCross.FavoritesController",
-		kind: enyo.Controller
-	});
 });
