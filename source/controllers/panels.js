e927c83ab873496cbc6be5f3db61fb2f04d44277
Dave Freeman <sugardave@gmail.com>
2013-05-06 19:00:09 -0500

    removed fail

--- source/controllers/panels.js
+++ source/controllers/panels.js
@@ -6,14 +6,7 @@ enyo.ready(function() {
 		index: 0,
 		panels: null, // this will get set by the Panels instance that uses it
 		getActive: function() {
-			this.log(this.panels.getActive());
 			return this.panels.getActive;
-		},
-		selectPanelByNameFoo: function() {
-			//return enyo.bind(this, this.panels.selectPanelByName);
-			this.log(arguments);
-			this.log(this.panels.selectPanelByName.apply(arguments));
-			return enyo.bind(this.panels.owner, this.panels.selectPanelByName);
 		}
 	});
 
